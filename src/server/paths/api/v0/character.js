b00f5b8bb816246c831566ea13d5214ef6e93435
mithiwithi <magney@gmail.com>
2019-05-30 16:03:02 -0700

    Add individual character methods to the store
    Add paths to retrieve individual characters from the API.
    Also check in swagger config (pending a way to host it).

--- src/server/paths/api/v0/character.js
+++ src/server/paths/api/v0/character.js
@@ -4,17 +4,42 @@ import jsonSerialize from 'serialize-javascript';
 class Character {
   constructor() {
     this.store = {
+      byId: {
+        a1: {
+          characterId: 'a1',
+          name: 'Armus',
+        },
+        b2: {
+          characterId: 'b2',
+          name: 'The Caretaker',
+        },
+      },
       ids: ['a1', 'b2'],
     };
   }
 
   getCharacterIds = (_req, res) => {
     res.type('application/json').send(jsonSerialize(this.store.ids));
-  }
+  };
+
+  getCharacterById = (req, res) => {
+    const { characterId } = req.params;
+    const character = this.store.byId[characterId];
+    if (!character) {
+      res
+        .status(404)
+        .type('application/json')
+        .end();
+      return;
+    }
+
+    res.type('application/json').send(jsonSerialize(character));
+  };
 
   initialize() {
     this.router = express.Router();
-    this.router.get('/ids', this.getCharacterIds);
+    this.router.get('/:characterId', this.getCharacterById);
+    this.router.get('/', this.getCharacterIds);
     return this;
   }
 }
