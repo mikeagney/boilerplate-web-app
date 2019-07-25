c57084c562be33eb54f879661d9d044e7e6061d0
mithiwithi <magney@gmail.com>
2019-07-25 15:26:55 -0700

    GH-28: Redo character list as left-side pills
    Use custom tab layout based on a column of character name buttons to
    select the desired character tab.
    
    This is in preparation for retrieving character IDs from the API and
    being able to dynamically grow the character name column/retrieve from
    the database in pages rather than all at once.

--- src/client/components/characters/character-list/character-list.component.js
+++ src/client/components/characters/character-list/character-list.component.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Tabs, Tab } from 'react-bootstrap';
+import {
+  Tab, Row, Col, Nav,
+} from 'react-bootstrap';
 import { MdAdd } from 'react-icons/md';
 import Character from '../character';
 import NewCharacter from '../new-character';
@@ -8,16 +10,34 @@ import NewCharacter from '../new-character';
 const CharacterList = ({
   characterIds, characterNames, selectedId, setSelected,
 }) => (
-  <Tabs className="character-list" activeKey={selectedId} onSelect={setSelected}>
-    {characterIds.map(id => (
-      <Tab key={id} eventKey={id} title={characterNames[id]}>
-        <Character characterId={id} selected={id === selectedId} />
-      </Tab>
-    ))}
-    <Tab title={<MdAdd />} eventKey="">
-      <NewCharacter />
-    </Tab>
-  </Tabs>
+  <Tab.Container className="character-list" activeKey={selectedId} onSelect={setSelected}>
+    <Row>
+      <Col sm={3}>
+        <Nav variant="pills" className="flex-column">
+          {characterIds.map(id => (
+            <Nav.Link key={id} eventKey={id}>
+              {characterNames[id]}
+            </Nav.Link>
+          ))}
+          <Nav.Link eventKey="">
+            <MdAdd />
+          </Nav.Link>
+        </Nav>
+      </Col>
+      <Col sm={9}>
+        <Tab.Content>
+          {characterIds.map(id => (
+            <Tab.Pane key={id} eventKey={id}>
+              <Character characterId={id} selected={id === selectedId} />
+            </Tab.Pane>
+          ))}
+          <Tab.Pane eventKey="">
+            <NewCharacter />
+          </Tab.Pane>
+        </Tab.Content>
+      </Col>
+    </Row>
+  </Tab.Container>
 );
 
 CharacterList.propTypes = {
